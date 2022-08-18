424f44347a1d9f6c1c43284cd2ad09d6858af3f3
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-08-18 18:43:23 -0400

    Added Files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 * Objects are the building blocks of which our Javascript applications are dependent.
+
 * Objects are variables that contain a combination of primitive data types.
 * Primitive Data Types: 
 1. Boolean
@@ -9,6 +10,7 @@
 6. Long
 7. Float
 8. Double
+
 * Ways to use functions within an object: 
 1. Key Value Pairs 
 2. Method: An internal function.
---
+++ script.js
@@ -0,0 +1,48 @@
+// Objects are a collection of values
+
+Object Type: Car 
+Properties = Variables 
+Properties: key/value pairs. 
+Keys; make, model, year. 
+Values: "Honda," "Civic," "2003." 
+//   key: "value",
+//   make: "Honda",
+//   model: "Civic",
+//   year: 2003,
+// }; 
+
+/* Adding a function */
+/* The alert that is displayed from variables */
+Displays: key-Value
+// var displayList = function () {
+//   alert("key: " + cars.key);
+// };
+
+Displays: make-Honda
+// var displayList = function () {
+//   alert("make: " + cars.make);
+// };
+
+Displays: model-Civic
+// var displayList = function () {
+//   alert("model: " + cars.model);
+// };
+
+Displays: year-2003
+// var displayList = function () {
+//   alert("year: " + cars.year);
+// };
+
+// displayList();
+
+Using method: an internal function.
+// var cars = {
+//   make: "Honda",
+//   model: "Civic",
+//   year: 2003,
+//   displayList: function () {
+//     alert("Make: " + cars.make);
+//   },
+// };
+
+// cars.displayList();
